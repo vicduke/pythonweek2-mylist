f33b3fb8ca5b10a6711b5f25eace56640cf18dff
victor <mutaivic10@gmail.com>
2024-03-07 08:14:36 +0300

    first commit

---
+++ mylist.py
@@ -0,0 +1,10 @@
+my_list=[]
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.insert(1,15)
+my_list.extend([50,60,70])
+del(my_list[-1])
+my_list.sort()
+print("Index of the value 30:",my_list.index(30))
